0dd752170d29ba55838ae1a91f6632436a4fef46
fiftydinar <65243233+fiftydinar@users.noreply.github.com>
2026-01-23 23:01:19 +0100

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -6,7 +6,7 @@ ARCH=$(uname -m)
 
 echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
-pacman -Syu --noconfirm sdl2 sdl12-compat pipewire-audio libdecor
+pacman -Syu --noconfirm sdl2 sdl12-compat libdecor
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
